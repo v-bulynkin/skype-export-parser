1b6a983d1e31d01d458b19b88685a683b998dae0
v-bulynkin <56349712+v-bulynkin@users.noreply.github.com>
2020-11-09 14:09:48 +0300

    Create skype-export-parser.ps1

---
+++ skype-export-parser.ps1
@@ -0,0 +1,60 @@
+$rootFolder = (read-host "Enter the Skype exported files folder path").trim() -replace "\\+$"
+$mediaFolder = dir "$rootFolder\media"
+# sort messages by time ascending
+$messages = (gc "$rootFolder\messages.json" -Encoding UTF8 |ConvertFrom-Json).conversations.messagelist |sort id
+$IDs = $messages.conversationid -match "^8:" |sort -Unique
+
+# Sort media by IDs with renaming
+$sorting = Read-Host "Type 1 and press enter if you want to sort media files and distribute them by folders"
+
+foreach ($ID in $IDs) {
+    # Exclude message service types, get timestamp string
+    $thread = $messages |? {$_.conversationid -eq "$ID" -and $_.messagetype -match 'text'} |select *,@{n='timestamp';e={$_.originalarrivaltime -as [datetime]}}
+
+    $IDdisplayName = $thread.displayname |sort -Unique
+    Write-Host -fore Green "Processing thread with $IDdisplayName..."
+
+    if ($sorting -eq 1) {mkdir "$rootFolder\$IDdisplayName" -ErrorAction SilentlyContinue > $null}
+
+    $threadHtml = foreach ($msg in $thread) {
+        # Is there media content and is it presented locally
+        if ($msg.amsreferences -and $mediaFolder -match $msg.amsreferences) {
+        $msgMediaJson,$msgMedia = ($mediaFolder -match $msg.amsreferences).where({$_.extension -eq '.json'}, 'split')
+        # what extension the media file has
+        $msgMediaExt = (gc "$($msgMediaJson.fullname)" |ConvertFrom-Json).filename -replace "^.*\."
+        # select the biggest one
+        $msgMediaFile = $msgMedia |sort length |select -last 1
+        # copy (not rename because you may need to start this script again)
+        copy "$($msgMediaFile.fullname)" "$rootFolder\media\$($msgMediaFile.basename).$msgMediaExt" -ErrorAction SilentlyContinue
+        $msgMediaPath = 'file:///' + "$rootFolder\media\$($msgMediaFile.basename).$msgMediaExt"
+            
+            if ($sorting -eq 1) {
+            Copy-Item "$($msgMediaFile.fullname)" "$rootFolder\$IDdisplayName\$(($msg.timestamp).tostring("yyyy-MM-dd hh-mm-ss")) $IDdisplayName.$msgMediaExt" -ErrorAction SilentlyContinue
+            }
+    
+            # Image or video
+            if ($msgMediaPath -match "jpe?g$|png$|gif$") {
+            $msgContent = $msg.content -replace "<URIObject.*URIObject>","<img src=`"$msgMediaPath`" style=`"max-width:640px;width:100%`">"
+            }
+            else {
+            $msgContent = $msg.content -replace "<URIObject.*URIObject>","<video controls style=`"max-width:640px;width:100%`"> <source src=`"$msgMediaPath`"  type=`"video/$msgMediaExt`" /></video>"
+            }
+        }
+        # If there are no media
+        else {
+        $msgContent = $msg.content
+        }
+    
+        # Highlight interlocutor posts (your displayname is not shown in conversations)
+        if ($msg.displayname) {
+        "<p style=`"background-color:#c9e4ff`"><i>$(($msg.timestamp).tostring()) $($msg.displayname)</i><br>$msgContent</p>"
+        }
+        else {
+        "<p><i>$(($msg.timestamp).tostring())</i><br>$msgContent</p>"
+        }
+    }
+            
+    # Write the HTML down
+    $threadHtml > "$rootFolder\$IDdisplayName.html"
+
+}
