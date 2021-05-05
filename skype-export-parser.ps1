be76091bf38050bd7704a1f73775d990b0dfc5e7
Viacheslav Bulynkin <56349712+v-bulynkin@users.noreply.github.com>
2021-05-05 22:40:23 +0300

    Unnecessary questions have been removed
    Autodetect TZ
    Always sort media files

--- skype-export-parser.ps1
+++ skype-export-parser.ps1
@@ -1,25 +1,24 @@
 # source folder
 $rootFolder = (read-host "Enter the Skype exported files folder path").trim() -replace "\\+$"
 # Time offset (all messages in Skype export have UTC timestamps so they need to be converted)
-$tz = (read-host "Enter your UTC offset, for example, 2.5 or -1. It should be decimal (i.e., -3:30 = -3.5, +8:45 = 8.75). For UTC itself enter 0 or nothing").trim()
+$tz = Get-TimeZone
+if ($tz.BaseUtcOffset.TotalMinutes -ne 0) {
+    Write-Host -fore Magenta "Messages timestamps will be recalculated to your timezone:`n$($tz.displayname)."
+}
 $mediaFolder = dir "$rootFolder\media"
 # sort messages by time ascending
 $messages = (gc "$rootFolder\messages.json" -Encoding UTF8 |ConvertFrom-Json).conversations.messagelist |sort id
 $IDs = $messages.conversationid -match "^8:" |sort -Unique
 
-# Sort media by IDs with renaming
-$sorting = Read-Host "Type 1 and press enter if you want to sort media files and distribute them by folders"
-
 foreach ($ID in $IDs) {
     # Exclude message service types, get timestamp string
     $thread = $messages |? {$_.conversationid -eq "$ID" -and $_.messagetype -match 'text'} |select *,
-    @{n='timestamp';e={if ($tz) {($_.originalarrivaltime -as [datetime]).addhours($tz)} else {$_.originalarrivaltime -as [datetime]}}}
-
+    @{n='timestamp';e={if ($tz.BaseUtcOffset.TotalMinutes -ne 0) {($_.originalarrivaltime -as [datetime]).addminutes($tz.BaseUtcOffset.TotalMinutes)} else {$_.originalarrivaltime -as [datetime]}}}
 
     $IDdisplayName = $thread.displayname |sort -Unique
     Write-Host -fore Green "Processing thread with $IDdisplayName..."
 
-    if ($sorting -eq 1) {mkdir "$rootFolder\$IDdisplayName" -ErrorAction SilentlyContinue > $null}
+    mkdir "$rootFolder\$IDdisplayName" -ErrorAction SilentlyContinue > $null
 
     $threadHtml = foreach ($msg in $thread) {
         # Is there media content and is it presented locally
@@ -33,10 +32,8 @@ foreach ($ID in $IDs) {
         copy "$($msgMediaFile.fullname)" "$rootFolder\media\$($msgMediaFile.basename).$msgMediaExt" -ErrorAction SilentlyContinue
         $msgMediaPath = "media\$($msgMediaFile.basename).$msgMediaExt"
             
-            if ($sorting -eq 1) {
-            Copy-Item "$($msgMediaFile.fullname)" "$rootFolder\$IDdisplayName\$(($msg.timestamp).tostring("yyyy-MM-dd HH-mm-ss")) $IDdisplayName.$msgMediaExt" -ErrorAction SilentlyContinue
-            }
-    
+        Copy-Item "$($msgMediaFile.fullname)" "$rootFolder\$IDdisplayName\$(($msg.timestamp).tostring("yyyy-MM-dd HH-mm-ss")) $IDdisplayName.$msgMediaExt" -ErrorAction SilentlyContinue
+            
             # Image or video
             if ($msgMediaPath -match "jpe?g$|png$|gif$") {
             $msgContent = $msg.content -replace "<URIObject.*URIObject>","<img src=`"$msgMediaPath`" style=`"max-width:640px;width:100%`">"
@@ -45,7 +42,7 @@ foreach ($ID in $IDs) {
             $msgContent = $msg.content -replace "<URIObject.*URIObject>","<video controls style=`"max-width:640px;width:100%`"> <source src=`"$msgMediaPath`"  type=`"video/$msgMediaExt`" /></video>"
             }
         }
-        # If there are no media
+        # If no media
         else {
         $msgContent = $msg.content
         }
